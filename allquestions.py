545bb84df09b117702b84d78ec01de1f5a85c132
srsanjaykumar <136292754+srsanjaykumar@users.noreply.github.com>
2023-06-18 15:35:31 +0530

    Create allquestions.py

---
+++ allquestions.py
@@ -0,0 +1,40 @@
+import frappe,hashlib
+from frappe.model.document import Document
+from frappe import _
+class ALLQuestions(Document):
+	def before_insert(self):
+		if(self.question):
+			dummy=hashlib.md5(self.question.encode()).hexdigest()
+			self.name=dummy
+			self.uid=dummy
+		else:
+			frappe.throw(_("Please Fill the Question "))
+	
+	def before_save(self):
+		dummy=hashlib.md5(self.question.encode()).hexdigest()
+		self.uid=dummy
+
+@frappe.whitelist(allow_guest=1)
+def get():
+	allCategoryNames=frappe.get_all("Category1")
+	dict={}
+	for category in allCategoryNames:
+		cat_name=category['name']
+		catdoc=frappe.get_list("ALLQuestions",filters={"category":cat_name});
+		doc_each_quest=[]
+		for cat in catdoc:
+			doc_make_id={}
+			part_doc=frappe.get_doc("ALLQuestions",cat['name'])
+			doc_make_id['id']=part_doc.uid
+			doc_make_id['question']=part_doc.question
+			length=len(part_doc.options)
+			num_of_option=[]
+			for i in range(length):
+				num_of_option.append("o"+str(i+1))
+			opt={}
+			for j in range(length):
+				opt[num_of_option[j]]=part_doc.options[j].proc_name
+			doc_make_id['values']=opt
+			doc_each_quest.append(doc_make_id)
+		dict[cat_name]=doc_each_quest
+	return dict
